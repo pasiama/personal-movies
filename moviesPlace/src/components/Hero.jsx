be9d37634554269e973163a2b02115fdd48934b7
Asiama Phoebe <phoebeasiama6@gmail.com>
2025-01-31 17:19:52 +0000

    Setup and HeroPage

---
+++ moviesPlace/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="hero flex">
+      <div className="pl-4 w-96 pt-6 mt-160 ml-54">
+        <button className="p-3 w-full text-center font-bold text-black tracking-widest  m-3 bg-amber-100 hover:bg-amber-200 capitalize rounded-full md:rounded-lg">
+          L O G I N
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
